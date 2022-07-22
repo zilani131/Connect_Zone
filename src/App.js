5cd36acf1e04291708525a628e8f176eea2d1b9e
zilani131 <zilani.cuet@gmail.com>
2022-07-22 12:01:48 +0600

    developement update

--- src/App.js
+++ src/App.js
@@ -4,20 +4,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className='bg-slate-500 border border-red-300'>zilani</h1>
+      <button className='btn btn-primary' > add</button>
     </div>
   );
 }
