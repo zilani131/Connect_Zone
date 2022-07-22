2c9437f3368a9b2e4c2e745ff2da64b2dd241a9b
zilani131 <zilani.cuet@gmail.com>
2022-07-22 16:10:40 +0600

    latest post

--- src/Page/Home/Home.js
+++ src/Page/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import LatestPost from './LatestPost';
 import Newsfeed from './Newsfeed';
 
 const Home = () => {
     return (
         <div>
             <Newsfeed></Newsfeed>
+            <LatestPost></LatestPost>
         </div>
     );
 };
